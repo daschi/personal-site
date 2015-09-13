232c6b04135fe65c25a35bb7dd10e0f82aa6374f
daschi <daschi87@gmail.com>
2015-09-13 15:45:53 -0700

    Add views to the contact page, landing page, and posts index and project index pages. Add styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150913193704) do
+ActiveRecord::Schema.define(version: 20150913220226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "blogs", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.string   "images"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -34,7 +26,6 @@ ActiveRecord::Schema.define(version: 20150913193704) do
   create_table "projects", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.string   "images"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
